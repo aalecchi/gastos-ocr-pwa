155c001b8bb0e43faa42ff7cfdf0eba7219e8fa6
aalecchi <43821115+aalecchi@users.noreply.github.com>
2025-10-25 10:43:45 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // App base de Gastos OCR
 // Reemplaza la URL del Web App de Google Apps Script:
-const WEB_APP_URL = 'YOUR_APPS_SCRIPT_WEB_APP_URL';
+const WEB_APP_URL = 'https://script.google.com/home/projects/1XFsuayp0ldTkY9w05YdVsmxIfKngs1qy4RBDX7r2Wt5cNVTKQM5EFcQr/edit';
 
 console.log('App OCR lista. Agrega tu lógica aquí.');
